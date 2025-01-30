20e1b58dfb54ca29dba698ec0b7f92dd148e8b7d
sophie <d00259237@student.dkit.ie>
2025-01-30 13:18:50 +0000

    Created BooksContext and configured DbSet properties for Users, Books, UserProgresses, and Reviews

---
+++ BookTrackerAPI/Models/BooksContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookTrackerAPI.Models
+{
+    public class BooksContext : DbContext
+    {
+        public string DbPath { get; set; }
+
+        public BooksContext(DbContextOptions<BooksContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Book> Books { get; set; } = null!;
+        public DbSet<UserProgress> UserProgresses { get; set; } = null!;
+        public DbSet<Review> Reviews { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+             new User
+             {
+                 Id = 1,
+                 Username = "Saoirse McGuinness",
+                 Email = "Saoirsemcg@yahoo.ie"
+             }
+                );
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    BookId = 1,
+                    Title = "Call me By your Name",
+                    Description = "",
+                    Author = "Andre Aciman",
+                    Genre = "Romance",
+                    PagesRead = 1,
+                }
+                );
+
+            modelBuilder.Entity<UserProgress>().HasData(
+                new UserProgress
+                {
+                    Id = 1,
+                    BookId = 1,
+                    UserId = 1,
+                    PagesRead = 100,
+                    LastUpdated = DateTime.Now
+                }
+                );
+
+            modelBuilder.Entity<Review>().HasData(
+                new Review
+                {
+                    Id = 1,
+                    UserId = 1,
+                    BookId = 1,
+                    Rating = 5,
+                    Comment = "The novel centers on the sudden and powerful romance between Elio and Oliver"
+                }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
