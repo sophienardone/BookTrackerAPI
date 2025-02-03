b9ca6b1ed8846cf80bfca0a4a75a9d8e3e8989d9
sophie <d00259237@student.dkit.ie>
2025-02-03 18:12:52 +0000

    Added username and title to UserProgressDTO to show corresponding user and book title

--- BookTrackerAPI/Controllers/UserProgressDTO.cs
+++ BookTrackerAPI/Controllers/UserProgressDTO.cs
@@ -2,8 +2,14 @@
 {
     public class UserProgressDTO
     {
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
         public int BookId { get; set; }
         public int PagesRead { get; set; }
         public DateTime LastUpdated { get; set; }
+        public string UserName { get; set; }
+        public string Title { get; set; }
+
     }
 }
