57febfa6ecd03aa919b165787bdd1b4244032f20
sophie <d00259237@student.dkit.ie>
2025-02-03 14:46:21 +0000

    Added helper method in UserDTO (that is referenced in the userscontroller) for easier API handling

--- BookTrackerAPI/Controllers/UserDTO.cs
+++ BookTrackerAPI/Controllers/UserDTO.cs
@@ -6,7 +6,14 @@
         public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public List<UserProgressDTO>? UserProgress { get; set; }
-        public List<ReviewDTO>? Reviews { get; set; }
+        public List<UserProgressDTO>? UserProgress { get; set; } = new List<UserProgressDTO>();
+        public List<ReviewDTO>? Reviews { get; set; } = new List<ReviewDTO>();
+
+        public UserDTO() { 
+        
+            UserProgress = new List<UserProgressDTO>();
+            Reviews = new List<ReviewDTO>();
+        
+        }
     }
 }
