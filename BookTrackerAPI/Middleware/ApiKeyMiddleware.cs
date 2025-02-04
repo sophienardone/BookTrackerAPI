214d89a4ec034be37c14412e372dabc91ccbd6d5
sophie <d00259237@student.dkit.ie>
2025-02-04 22:42:02 +0000

    Created Middleware folder and created a new class for creating my own api key

---
+++ BookTrackerAPI/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,33 @@
+﻿namespace BookTrackerAPI.Middleware
+{ }
+
+//https://stackoverflow.com/questions/70277577/asp-net-core-simple-api-key-authentication
+public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(!context.Request.Headers.TryGetValue("X-API-KEY", out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("API Key is missing");
+                return;
+            }
+
+            var AdminKey = _configuration["ApiKeys:AdminKey"];
+            if(AdminKey != extractedApiKey) {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Invalid API Key");
+        }
+
+            await _next(context);
+    }
+}
